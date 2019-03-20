6ac29e30ae1fb03422fd0ef23effe1ef98a44652
SOUNISH NATH <flock.sinasini@gmail.com>
2019-03-21 01:01:58 +0530

    edit sucks

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import './landing_code.dart';
 import './quiz_page.dart';
+import './score_page.dart';
 import './UI/answer_button.dart';
 
 Color primary_Color = const Color.fromRGBO(3, 54, 255,1);
@@ -9,6 +10,6 @@ Color scaffold_back_color = const Color.fromRGBO(250, 22 ,3, 1);
 void main() {
   runApp(new MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: new LandingPage(),
+    home: new ScorePage()
   ));
 }
--- lib/score_page.dart
+++ lib/score_page.dart
@@ -1,2 +1,31 @@
 import 'package:flutter/material.dart';
 
+class ScorePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Material(
+      shadowColor: Colors.pinkAccent,
+      color: Colors.pink,
+      child: new Column(
+        mainAxisAlignment:  MainAxisAlignment.center,
+        children: <Widget>[
+          new Text("Your score ",
+          style: new TextStyle(
+            fontSize: 22.0,
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
+          ),
+          ),
+          new Text("3/10"),
+          new IconButton(
+            new Icon(Icons.arrow_right),
+            iconSize: 48.0,
+            color: Colors.white,
+
+          ),
+        ],
+      ),
+    );
+  }
+}
